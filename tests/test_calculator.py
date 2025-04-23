f5919915a559cbe8cc8b0eecd554a393ae64962d
boom2831 <bhumikag.btech23@rvu.edu.in>
2025-04-23 12:27:41 +0530

    Initial Commit

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -28,16 +28,6 @@ class TestCalculator(unittest.TestCase):
     def test_subtract(self):
         self.assertEqual(subtract(10, 6), 4)
 
-    def test_multiply(self):
-        self.assertEqual(multiply(4, 5), 20)
-
-    def test_divide(self):
-        self.assertEqual(divide(9, 3), 3.0)
-
-    def test_divide_by_zero(self):
-        with self.assertRaises(ValueError):
-            divide(5, 0)
-
 if __name__ == "__main__":
     unittest.main()
 
